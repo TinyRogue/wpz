8e7667ca82d0d998990e5b9fff4715e6d45985f4
Sou00 <igorlonak@gmail.com>
2023-06-18 14:11:40 +0200

    openai translation + saving and getting translated phrases

--- be/ebook/ebook.py
+++ be/ebook/ebook.py
@@ -1,6 +1,11 @@
-from flask import Flask
+import os
+
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 from firebase_admin import credentials, firestore, initialize_app
+import openai
+
+openai.api_key = os.environ["OPENAI_API_KEY"]
 
 cred = credentials.Certificate("wpz-ebook-firebase-adminsdk-dpdbt-1d6d7d1d23.json")
 default_app = initialize_app(cred)
@@ -15,11 +20,54 @@ def hello_world():
     return "Hello, cross-origin-world!"
 
 
-@app.route("/firestore")
-def firestore_test():
-    print(db.collection('test').add({'title': 'wiedzmin'}))
-    res = db.collection('test').document('1').get().to_dict()
-    return res
+@app.route("/books", methods=['POST'])
+def write():
+    try:
+        db.collection('books').add(request.get_json())
+        return "Succesfully added!"
+    except Exception as e:
+        return e
+
+
+@app.route("/books", methods=['GET'])
+def read():
+    try:
+        books = db.collection('books').where("userId", "==", request.get_json()['userId']).stream()
+        res = []
+        for book in books:
+            res.append(book.to_dict())
+        return jsonify(res)
+    except Exception as e:
+        return e
+
+
+@app.route("/phrases", methods=['GET'])
+def getphrases():
+    try:
+        phrases = db.collection('phrases').where("userId", "==", request.get_json()['userId']).stream()
+        res = []
+        for phrase in phrases:
+            res.append(phrase.to_dict())
+        return jsonify(res)
+    except Exception as e:
+        return e
+
+
+@app.route("/phrases", methods=['POST'])
+def translate():
+    messages = []
+    req = request.get_json()
+    try:
+        messages.append({"role": "user", "content": "Please translate " + req['phrase'] + " into "+req['language']})
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=messages
+        )
+        tr = response["choices"][0]["message"]["content"]
+        db.collection('phrases').add({'phrase': req['phrase'], 'language': req['language'], 'translation': tr, 'userId': req['userId']})
+        return "Translated"
+    except Exception as e:
+        return e
 
 
 if __name__ == '__main__':
