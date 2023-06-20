97f333dae2626ef43de0e659bf590606af63a2f8
msobolewska <magdalena.sobolewskaE3@gmail.com>
2023-06-21 01:29:41 +0200

    added post on /user/userId/books

--- be/ebook/ebook.py
+++ be/ebook/ebook.py
@@ -1,15 +1,21 @@
 import os
 
+import firebase_admin
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from firebase_admin import credentials, firestore, initialize_app
+from firebase_admin import credentials, firestore, initialize_app, storage
 import openai
 
 openai.api_key = os.environ["OPENAI_API_KEY"]
 
 cred = credentials.Certificate("wpz-ebook-firebase-adminsdk-dpdbt-1d6d7d1d23.json")
-default_app = initialize_app(cred)
+default_app = initialize_app(cred, {
+    'storageBucket': 'wpz-ebook.appspot.com'
+})
+
 db = firestore.client(default_app)
+bucket = storage.bucket()
+
 
 app = Flask(__name__)
 CORS(app)
@@ -39,6 +45,39 @@ def read(userId):
         return jsonify(res)
     except Exception as e:
         return e
+
+
+@app.route("/user/<string:userId>/books", methods=['POST'])
+def add(userId):
+    if 'file' not in request.files:
+        return 'No file uploaded.', 400
+
+    file = request.files['file']
+
+    # Validate the file extension
+    if file.filename.endswith('.epub'):
+        # Save the file to a desired location
+
+        if not os.path.exists('tmp'):
+            os.makedirs('tmp')
+
+        temp_path = f'tmp/{file.filename}'
+        file.save(temp_path)
+
+        blob = bucket.blob(file.filename)
+        blob.upload_from_filename(temp_path)
+
+        os.remove(temp_path)
+
+        document_data = {
+            'filename': file.filename,
+            'storage_path': f'gs://wpz-ebook.appspot.com/{file.filename}'
+        }
+        db.collection('files').add(document_data)
+
+        return 'File uploaded successfully.'
+    else:
+        return 'Invalid file format. Please upload an EPUB file.', 400
 
 
 @app.route("/user/<string:userId>/phrases", methods=['GET'])
